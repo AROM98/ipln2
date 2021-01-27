524bdc884e4972847d240f174d3d66e724cce61f
AROM98 <rafa.olim@sapo.pt>
2021-01-27 10:04:39 +0000

    update 4

--- rimas.py
+++ rimas.py
@@ -9,18 +9,37 @@ import pandas as pd
 
 os.system("date")
 
-ops,args = getopt(sys.argv[1:],"n: ")
+ops,args = getopt(sys.argv[1:],"pn: ")
 ops = dict(ops)
 
+def rima_palavra():
+    #pipe = subprocess.Popen(["perl", "coisa2"], stdout=sys.stdin)
+    #os.system("./coisa2")
+    print("digite uma palavra:")
+    output = subprocess.check_output("./coisa2", shell=True)
+    print('agora vou imprimir output:')
+    print(output)
+
+def rima_poema():
+    return
+####  limpar output   -  importante
+
+# comparar com palavras da "wordlist-rimas.txt"
+
+# fazer reguex para encontrar silaba tónica e silaba final
+
+# usar vogal da tónica e ultima silaba.
+
+# palavras que riamam têm estes 2 campos iguais
+
+# resolver casos com apenas 1 silaba
 
 
-#pipe = subprocess.Popen(["perl", "coisa2"], stdout=sys.stdin)
-
-#os.system("./coisa2")
-output = subprocess.check_output("./coisa2", shell=True)
-print('agora vou imprimir output:')
-print(output)
 
 
-
-
+if '-n' in ops:
+    print('nome do ficheiro do poema = ', ops['-n'])
+elif '-p' in ops:
+    rima_palavra()
+else:
+    print('no no no, plz indicar -p para palavra ou -n para poema!')
